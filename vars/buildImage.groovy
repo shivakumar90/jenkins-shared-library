9143c545dd6840f086bc27e48a8e529812cfb698
shivakumar90 <chenreddyshivakumar1@gmail.com>
2025-05-22 20:18:00 +0530

    Added params to the groovy files

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,8 +1,8 @@
-def call() {
+def call(String imageName) {
     echo "building the application..."
     withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', usernameVariable: "USER", passwordVariable: "PASS")]) {
-        sh "docker build -t shivakumarreddy1/demo-app:jma-3.0 ."
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh "docker push shivakumarreddy1/demo-app:jma-3.0"
+        sh "docker push $imageName"
     }
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,4 +1,4 @@
 def call() {
-    echo "building the application..."
+    echo "building the application... for branch $BRANCH_NAME"
     sh 'mvn package'
 }
